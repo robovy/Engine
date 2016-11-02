c1a94ef501128c697c1a16302f1413f958d1afb6
robovy <robovystudios@gmail.com>
2016-11-02 18:23:51 -0400

    Update main.cpp

--- SourceFiles/main.cpp
+++ SourceFiles/main.cpp
@@ -6,6 +6,9 @@ int main()
         using namespace graphics;
         
         Window window("Engine Test", 800, 600);
+        glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+        
+        std::cout << glGetString(GL_VERSION); << std::endl;
         
         while(!window.closed())
         {
