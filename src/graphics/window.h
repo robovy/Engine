5b0ef8a7a207f783bf571ed4f7194c489fc88ae1
robovy <robovystudios@gmail.com>
2016-11-02 11:01:21 -0400

    Update window.h

--- src/graphics/window.h
+++ src/graphics/window.h
@@ -2,5 +2,17 @@
 
 namespace engine
 {
-
+        namespace graphics
+        {
+                class Window
+                {
+                        private:
+                                const char *name;
+                                int width, height;
+                        public:
+                                Window(const char *name, int width, int height);
+                                ~Window();
+                                void update() const;
+                };
+        }
 }
