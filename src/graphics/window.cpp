99156b08e8f01620b7d9d6b3edec721fd303278d
robovy <robovystudios@gmail.com>
2016-11-02 14:31:24 -0400

    Update window.cpp

--- src/graphics/window.cpp
+++ src/graphics/window.cpp
@@ -6,7 +6,9 @@ namespace engine
         {
                 Window::Window(const char *name int width int height)
                 {
-                  
+                        m_Name = name;
+                        m_Width = width;
+                        m_Height = height;
                 }
         }
 }
