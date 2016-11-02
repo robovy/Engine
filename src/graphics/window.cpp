ffd44fc60e74c2284c5d81ae571dbe7450d632e5
robovy <robovystudios@gmail.com>
2016-11-02 17:37:13 -0400

    Update window.cpp

--- src/graphics/window.cpp
+++ src/graphics/window.cpp
@@ -4,11 +4,48 @@ namespace engine
 {
         namespace graphics
         {
-                Window::Window(const char *name int width int height)
+                Window::Window(const char *title int width int height)
                 {
-                        m_Name = name;
+                        m_Title = title;
                         m_Width = width;
                         m_Height = height;
+                        if(!init())
+                                glfwTerminate();
+                }
+                
+                Window::~Window()
+                {
+                        glfwTerminate();
+                }
+                
+                bool Window::init()
+                {
+                        if(!glfwInit())
+                        {
+                                std::cout << "Error" << std::endl;
+                                return false;
+                        }
+
+                        m_Window = glfwCreateWindow(m_Width, m_Height, NULL, NULL);
+                        if(!m_Window)
+                        {
+                                glfwTerminate();
+                                std::cout << "Could Not Create glfw Window" << std::endl;
+                                return;
+                        }
+                        gflwMakeContextCurrent(m_Window);
+                        return true;
+                }
+                
+                bool Window::closed()
+                {
+                        glfwWindowShouldClose(m_Window );
+                }
+                
+                void Window::update() const
+                {
+                        glfwPollEvents();
+                        glfwSwapBuffers(m_Window);
                 }
         }
 }
