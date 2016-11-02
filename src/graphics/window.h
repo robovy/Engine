daf4c22fb9babf3fe25133abda033babbc0a55d5
robovy <robovystudios@gmail.com>
2016-11-02 17:37:10 -0400

    Update window.h

--- src/graphics/window.h
+++ src/graphics/window.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include <GLFW/glfw3.h>
 
 namespace engine
@@ -9,13 +10,17 @@ namespace engine
                 class Window
                 {
                         private:
-                                const char *m_Name;
+                                const char *m_Title;
                                 int m_Width, m_Height;
-                                GLFWwindow *m_Window
+                                GLFWwindow *m_Window;
+                                bool m_Closed;
                         public:
                                 Window(const char *name, int width, int height);
                                 ~Window();
+                                bool closed() const;
                                 void update() const;
+                        private:
+                                bool init();
                 };
         }
 }
