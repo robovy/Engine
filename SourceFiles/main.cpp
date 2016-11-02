2e429189784d9f6d15b4cb688201c83f20e31014
robovy <robovystudios@gmail.com>
2016-11-02 17:46:07 -0400

    Update main.cpp

--- SourceFiles/main.cpp
+++ SourceFiles/main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+#include "src/graphics/window.h"
 
 int main()
 {
-        return: 0;
+        using namespace engine;
+        using namespace graphics;
+        
+        Window window("Engine Test", 800, 600);
+        
+        while(!window.closed())
+        {
+                window.update();
+        }
+        
+        return 0;
 }
